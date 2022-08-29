8e1defa5212d03ec94fdf71d97c3368aceafd0e4
Rafał Całka <calka.rafal@gmail.com>
2022-08-29 17:26:27 +0200

    Multiple API requests when the list of files is larger than 30

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,53 @@
+// External dependencies
 import * as core from '@actions/core';
 import Cloudflare from 'cloudflare';
 import Sitemapper from 'sitemapper';
+import {chunk, uniq} from 'lodash-es';
+
+// Internal dependencies
 import checkAuthInput from './checkAuthInput';
 
+const zoneId = core.getInput('zone', {required: true});
+
+const apiToken = core.getInput('api_token');
+
+const email = core.getInput('email');
+const apiKey = core.getInput('api_key');
+
+checkAuthInput(apiToken, email, apiKey);
+
+//
+const client = new Cloudflare({
+  token: apiToken.length === 0 ? undefined : apiToken,
+  email: email.length === 0 ? undefined : email,
+  key: apiKey.length === 0 ? undefined : apiKey
+});
+
+//
+const files = core.getInput('files');
+let filesArray: string[] = [];
+
+if (files.length) {
+  filesArray = files.split(' ');
+}
+
+function purgeCache(files: string[]) {
+  client.zones
+    .purgeCache(zoneId, {
+      //@ts-ignore missing purge_everything type - https://github.com/cloudflare/node-cloudflare/pull/107
+      purge_everything: files.length === 0 ? true : undefined,
+      files: files.length === 0 ? undefined : files
+    })
+    .then(response => {
+      console.log('Purged: ', files.join(', '));
+    })
+    .catch(error => {
+      console.error(error);
+      core.setFailed(error.message);
+    });
+}
+
 async function run() {
-  const zoneId = core.getInput('zone', {required: true});
-
-  const apiToken = core.getInput('api_token');
-
-  const email = core.getInput('email');
-  const apiKey = core.getInput('api_key');
-
-  checkAuthInput(apiToken, email, apiKey);
-
-  //
-  const files = core.getInput('files');
-  let filesArray: string[] = [];
-
-  if (files.length) {
-    filesArray = files.split(' ');
-  }
-
   //
   const sitemapUrl = core.getInput('sitemap');
   if (sitemapUrl.length) {
@@ -29,36 +56,28 @@ async function run() {
     await sitemap
       .fetch()
       .then(({sites}) => {
+        console.log(`Found ${sites.length} URLs in the sitemap`);
         filesArray = filesArray.concat(sites);
-        //Cloudflare's API allows you to clear only 30 URLs at once
-        //TODO
-        filesArray.length = 30;
-        console.log(filesArray);
       })
       .catch(error => {
         console.error(error);
       });
   }
 
-  const client = new Cloudflare({
-    token: apiToken.length === 0 ? undefined : apiToken,
-    email: email.length === 0 ? undefined : email,
-    key: apiKey.length === 0 ? undefined : apiKey
-  });
+  if (filesArray.length) {
+    // Remove duplicates
+    filesArray = uniq(filesArray);
+  }
 
-  client.zones
-    .purgeCache(zoneId, {
-      //@ts-ignore missing purge_everything type - https://github.com/cloudflare/node-cloudflare/pull/107
-      purge_everything: filesArray.length === 0 ? true : undefined,
-      files: filesArray.length === 0 ? undefined : filesArray
-    })
-    .then(response => {
-      console.log(response);
-    })
-    .catch(error => {
-      console.error(error);
-      core.setFailed(error.message);
+  if (filesArray.length > 30) {
+    // Cloudflare's API allows you to clear only 30 URLs at once
+    const chunks = chunk(filesArray, 30);
+    chunks.forEach(chunk => {
+      purgeCache(chunk);
     });
+  } else {
+    purgeCache(filesArray);
+  }
 }
 
 run();
